44ce2a747cec4b543c01b6d41d48b8b06ac6e7af
DmytroVons <dmytrovons@gmail.com>
2023-08-14 14:24:11 +0300

    added the http library to the auth script

--- pdf_ai_assistant/auth.py
+++ pdf_ai_assistant/auth.py
@@ -2,7 +2,7 @@ from os import getenv
 
 from fastapi import Security, HTTPException
 from fastapi.security.api_key import APIKeyHeader
-from starlette.status import HTTP_403_FORBIDDEN
+from http import HTTPStatus
 
 api_key_header = APIKeyHeader(name="access_token", auto_error=False)
 
@@ -12,5 +12,5 @@ def get_api_key(api_key_header: str = Security(api_key_header)):
         return api_key_header
     else:
         raise HTTPException(
-            status_code=HTTP_403_FORBIDDEN, detail="Could not validate API KEY"
+            status_code=HTTPStatus.FORBIDDEN, detail="Could not validate API KEY"
         )
